d963a4fd213fa0e9216afc3caadfcdfd2a162b68
tomalex123 <aleksart13@yandex.ru>
2025-08-24 00:28:03 +0400

    created JS file and modified HTML file. Created tab logic with show/hide functions and used event delegation.

---
+++ js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", function () {
+  const tabHeadersParent = document.querySelector(".tabheader__items");
+  const tabHeaders = tabHeadersParent.querySelectorAll(".tabheader__item");
+  const tabContents = document.querySelectorAll(".tabcontent");
+
+  // function for hiddening all tabs and all active classes
+  function hideTabContentsAndActiveClasses() {
+    // tabContents.forEach((item) =>{
+    //     item.style.display = "none"
+    // })
+    // tabHeaders.forEach((item) =>{
+    //     item.classList.remove("tabheader__item_active")
+    // })
+
+    for (let i = 0; i < tabContents.length; i++) {
+      tabContents[i].style.display = "none";
+      tabHeaders[i].classList.remove("tabheader__item_active");
+    }
+  }
+
+  // function for showing current tab and adding active class for current header
+  function showTabContentsAndActiveClasses(i = 0) {
+    tabContents[i].style.display = "block";
+    tabHeaders[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContentsAndActiveClasses();
+  showTabContentsAndActiveClasses();
+
+  // event delegation
+  tabHeadersParent.addEventListener("click", (e) => {
+    if (e.target && e.target.matches(".tabheader__item")) {
+      for (let i = 0; i < tabHeaders.length; i++) {
+        if (e.target == tabHeaders[i]) {
+          hideTabContentsAndActiveClasses();
+          showTabContentsAndActiveClasses(i);
+        } else {
+        }
+      }
+    }
+  });
+});
